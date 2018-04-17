3018b5299a3e2af41537978a1124384932c029c8
Shota Matsuda <shota@takram.com>
2018-04-17 13:18:24 +0900

    updated project setup

--- test/fixture.js
+++ test/fixture.js
@@ -2,8 +2,8 @@
 // Copyright (C) 2016-Present Shota Matsuda
 
 /* global Planck */
+/* eslint-env worker */
 
-// eslint-disable-next-line no-restricted-globals
 self.importScripts(
   '/node_modules/babel-polyfill/dist/polyfill.js',
   '/node_modules/three/build/three.js',
--- webpack.config.unit.js
+++ webpack.config.unit.js
@@ -4,26 +4,25 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const pkg = require('./package.json')
-
 module.exports = {
+  mode: 'development',
   entry: [
-    path.resolve(__dirname, 'test', 'unit.js'),
+    path.resolve(__dirname, 'test/unit.js'),
   ],
   output: {
-    filename: 'webpack.js',
-    path: path.resolve(__dirname, 'dist', 'test', 'unit'),
+    path: path.resolve(__dirname, 'dist/test/unit'),
+    filename: 'webpack.js'
   },
   devtool: 'source-map',
   externals: {
+    'chai': 'chai',
+    'mocha': 'mocha',
     'source-map-support/register': 'null',
     '@takram/planck-core': 'Planck',
     'd3-array': 'd3',
     'd3-geo': 'd3',
     'd3-geo-projection': 'd3',
     'three': 'THREE',
-    'chai': 'chai',
-    'mocha': 'mocha',
   },
   module: {
     rules: [
@@ -39,6 +38,7 @@ module.exports = {
                 'es2016',
                 'es2017',
                 'stage-3',
+                'stage-2',
               ],
             },
           },
