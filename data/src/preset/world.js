de7ac5d28729a655d0553101860424527c2726ad
Shota Matsuda <shota@takram.com>
2018-06-13 12:25:55 +0900

    linted data source files

--- data/src/preset/world.js
+++ data/src/preset/world.js
@@ -4,13 +4,13 @@
 // http://www.naturalearthdata.com/downloads/10m-cultural-vectors
 export const url = 'http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip'
 
-export function transform(properties) {
+export function transform (properties) {
   const {
     ADM0_A3: countryCode,
-    NAME: countryName,
+    NAME: countryName
   } = properties
   return {
     countryCode,
-    countryName,
+    countryName
   }
 }
